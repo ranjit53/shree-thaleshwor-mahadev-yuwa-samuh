e1e2c2b84e07367884d6af3b381d991e487ff7a2
ranjit53 <ranjitsingh88219@gmail.com>
2025-11-11 14:53:55 +0300

    Update generate-password-hash.js

--- scripts/generate-password-hash.js
+++ scripts/generate-password-hash.js
@@ -5,12 +5,13 @@
 
 const bcrypt = require('bcryptjs');
 
-const password = process.argv[2] || 'admin12345';
+const password = process.argv[2] || 'admin123';
 
 bcrypt.hash(password, 10).then(hash => {
   console.log('Password:', password);
   console.log('Hash:', hash);
   console.log('\nUpdate data/settings.json with this hash for the admin user.');
 });
+
 
 
